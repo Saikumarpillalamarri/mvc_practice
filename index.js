dca0dfbfa76fdb8c6e84471a18c2c5c177aeed42
saikumarpillalamarri <saikumarpillalamarri222@gmail.com>
2024-11-06 10:54:51 +0530

    changes

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 var express = require("express");
+var mysql2 = require("mysql2");
+
 var app = express();
 
+var conn = mysql2.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"root",
+    database:"employee"
+})
 app.get("/",(req,res)=>{
     res.send("hello")
 })
 
+conn.connect((err)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log("connected to database")
+    }
+})
 var port = 3000;
 
 app.listen(port,()=>{
